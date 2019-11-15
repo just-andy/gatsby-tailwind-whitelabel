4bca372407433b18f9c295d484e3dac19b75c9ea
Andrew Cetnarskyj <andy@gunkdesign.co.uk>
2019-11-15 08:24:53 +0000

    navigation
    updated navigation links

--- src/components/modules/Navigation.js
+++ src/components/modules/Navigation.js
@@ -4,12 +4,18 @@ import { Link } from "gatsby"
 const Navigation = () => {
   return (
     <nav>
-      <ul>
+      <ul className="flex justify-between">
         <li>
           <Link to="">Home</Link>
         </li>
         <li>
           <Link to="">About</Link>
+        </li>
+        <li>
+          <Link to="">Prices</Link>
+        </li>
+        <li>
+          <Link to="">Contact</Link>
         </li>
       </ul>
     </nav>
