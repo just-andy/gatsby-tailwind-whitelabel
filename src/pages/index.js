58f5bbbde22535bb273f5a697600580776c332a3
Andy Cetnarskyj WM <andrew.cetnarskyj@woodmac.com>
2019-11-21 15:07:15 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default () => (
       roots in a piece of classical Latin literature from 45 BC, making it over
       2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
     </p>
-<DualCards /<
+    <DualCards />
     <ul>
       <li>List Item</li>
       <li>List Item</li>
