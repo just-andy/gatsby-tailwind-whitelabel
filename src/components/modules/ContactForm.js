7b410ff5afd927ed065cafa62a8c76349e7d80c2
Andys Macbook Pro <andy@just-andy.uk>
2020-06-01 21:20:07 +0100

    added first part of the formik, not connected with Netlify

--- src/components/modules/ContactForm.js
+++ src/components/modules/ContactForm.js
@@ -1,31 +1,71 @@
 import React from "react"
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
-
+const ErrorText = ({children})=>{
+  return (
+    <div className="text-accent-700 text-warning">{children}</div>
+  )
+}
 
 const ContactForm = () => {
   return (
-
-    <form className="mb-6">
+    <Formik
+    initialValues={{
+      name: '',
+      email: '',
+      message: '',
+      subject: ''
+    }}
+    validate={values => {
+      const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+      const errors = {};
+      if(!values.name) {
+        errors.name = 'Name Required'
+      }
+      if(!values.email || !emailRegex.test(values.email)) {
+        errors.email = 'Valid Email Required'
+      }
+      if(!values.subject) {
+        errors.subject = 'Subject Required'
+      }
+      if(!values.message) {
+        errors.message = 'Message Required'
+      }
+      return errors;
+    }}
+    onSubmit={(values, actions) => {
+      alert(JSON.stringify(values, null, 2));
+      actions.setSubmitting(false);
+    }}
+  >
+  {({ isSubmitting }) => (
+    <Form className="mb-6">
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="name">Your Name</label>
-        <input className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="text" name="name" placeholder="Your name" />
+        <Field className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" name="name" />
+        <ErrorMessage component={ErrorText} name="name" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="email">Your Email</label>
-        <input className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" placeholder="Email address" />
+        <Field className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" name="email" />
+        <ErrorMessage component={ErrorText} name="email" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="subject">Subject</label>
-        <input className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" type="text" name="subject" placeholder="If we need to call you" />
+        <Field nput className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" name="subject" />
+        <ErrorMessage component={ErrorText} name="subject" />
       </div>
       <div className="mb-4">
         <label className="block text-sm font-bold mb-2" htmlFor="textarea">Message</label>
-        <textarea className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" rows="6" name="textarea"></textarea>
+        <Field className="appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" component="textarea" name="textarea" />
+        <ErrorMessage component={ErrorText} name="textarea" />
       </div>
       <div className="mb-4">
-        <button type="submit" className="btn btn-primary">Send message</button>
+        <button type="submit" disabled={isSubmitting} className="btn btn-primary">Send message</button>
       </div>
-    </form>
+    </Form>
+  )}
+  </Formik>
   )
 }
 
